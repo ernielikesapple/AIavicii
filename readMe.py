af80311a9326d8be547b83a03063fbff8e338c8d
Ernie <jasonernie03@gmail.com>
2020-10-25 23:00:31 -0400

    try to find what data we need to study on

---
+++ readMe.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 25 16:08:35 2020
+
+@author: ernie
+"""
+
+'''
+read.me 
+
+
+current demo mainly based EDM tracks, currently I am trying to find out the 
+
+inner relationship why the lead melody and vocal meldoy 
+in 
+avicii, kygo, mike williams, brooks, mesto, retrovision's
+track is catchy
+
+
+
+
+
+
+
+
+
+
+
+'''
--- songSources.py
+++ songSources.py
@@ -6,3 +6,25 @@ Created on Sun Oct 25 16:01:48 2020
 @author: ernie
 """
 
+dataSources = { 'Avicii': {'waiting for love': 743231909},     # producer, track, play times on spotify 
+                'Mike Williams': {'key_2': 'value_2'},
+                'kygo': {'key_2': 'value_2'},
+                'brooks': {'key_2': 'value_2'},
+                'mesto': {'key_2': 'value_2'},
+                'retrovision' : {'key_2': 'value_2'},
+                }
+
+
+
+
+
+
+# rank from the spotify players
+
+
+
+
+
+
+
+# rank from myself
